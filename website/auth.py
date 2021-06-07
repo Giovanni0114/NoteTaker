b98dfd9a38358c385913d9e18d62cc0eaaeb6ee4
Giovanni1414 <janko.juroszek@gmail.com>
2021-06-07 10:28:56 +0200

    Add new feature, flasching messages

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,30 @@ def login():
 def logout():
     return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash("Email must be longer then 4 characters", category='error')
+            pass
+        elif len(firstName) < 2:
+            flash("First name must be longer then 2 characters", category='error')
+            pass
+        elif password1 != password2:
+            flash("Passwords don\'t match", category='error')
+            pass
+        elif len(password1) < 7:
+            flash("Password must be longer then 7 characters", category='error')
+            pass
+        else:
+            flash("Accout has been created succesfully", category='success')
+            #TODO add user to database
+            pass
+
+        
     return render_template("sign_up.html")
