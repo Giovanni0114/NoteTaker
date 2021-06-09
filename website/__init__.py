f01ac748c3683ab7cfc011f22d866f2b2fc1982e
Giovanni1414 <janko.juroszek@gmail.com>
2021-06-09 11:41:36 +0200

    Repair some code

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
+def create_database(app):
+    if not path.exists(f'website/{DB_NAME}'):
+        db.create_all(app=app)
+        print("Database has been created successfully")
 
 
 def create_app():
@@ -17,6 +23,9 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    return app
+    from .models import User, Note  
 
+    create_database(app)
+
+    return app
 
